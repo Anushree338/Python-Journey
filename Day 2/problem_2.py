1fe7fdb0e9375166330876a543125f238009077a
Anushree Bonde <anushreebonde338@gmail.com>
2026-06-04 23:49:32 +0530

    Add files via upload

---
+++ Day 2/problem_2.py
@@ -0,0 +1,15 @@
+#check the type of the variable assign using input function
+
+a = input("enter the value of a: ")
+
+print(type(a))
+
+
+
+
+
+# a= input("enter the value of a: ")
+
+# b= float(a)
+# t= type(b)
+# print(t)
