ea51e48d7ae78f5b13dea666e8171bf3f5f6070f
Anushree Bonde <anushreebonde338@gmail.com>
2026-06-16 22:41:04 +0530

    Add files via upload

---
+++ Day 9/5_with.py
@@ -0,0 +1,9 @@
+f = open("file.txt")
+print(f.read())
+f.close()
+
+#the same can be written using the statement like this
+with open("file.txt") as f:
+    print(f.read())
+
+    # you dont have to explicity close the file
