9eb36fb4be41e3cd34840b9f5edbeccc4cbef7ac
Anushree Bonde <anushreebonde338@gmail.com>
2026-06-25 22:07:56 +0530

    Add files via upload

---
+++ Day 12/6_input.py
@@ -0,0 +1,17 @@
+a = int(input("Enter a number a: "))
+
+b = int(input("Enter a number b: "))
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+print("sum is: ", a+b)
+
+# a = (input("Enter a number a: "))
+
+# b = (input("Enter a number b: "))
+
+# print("Number a is: ", a)
+# print("Number b is: ", b)
+# print("sum is: ", a+b)
+
+#it takes string so we need to put "int" before input to convert it to integer and then we can perform addition.
---
+++ Day 12/problem _5.py
@@ -0,0 +1,4 @@
+#calculate square of number enter by user
+
+a = int(input("enter your number: "))
+print("square of a is: ", a**2)  #a^2 is invalid in python or we can write a*a
